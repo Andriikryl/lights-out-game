9c84f0a7e713330585f4f73f3e311c6f1819b4cf
Andriikryl <andreyk557@gmail.com>
2023-04-16 20:13:57 +0300

    feat[board-component]

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const Board = () => {
-  return <div>test</div>;
+  const createGrid = () =>
+    new Array(5)
+      .fill()
+      .map((r) => new Array(5).fill().map((c) => Math.random() < 0.4));
+  const [board, setBoard] = useState(createGrid());
+
+  return (
+    <div className="Board">
+      {board.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((cell, colIndex) => (
+            <div key={`${rowIndex}-${colIndex}`}>
+              {rowIndex}
+              {colIndex}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Board;
